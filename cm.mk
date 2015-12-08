14c8049512a5e0b201aa0ea48e9674862efb59f0
Evasii0n <thomaswalker31200@gmail.com>
2015-12-08 17:56:54 +0000

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,9 +1,28 @@
+#
+# Copyright (C) 2013 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit some common Omni stuff
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -11,10 +30,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
-
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_angler
+PRODUCT_NAME := omni_angler
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
 
